73fed390426f9f01c8fe9342f3d4ad0f979c4943
TheMinmusDerp <71770907+TheMinmusDerp@users.noreply.github.com>
2021-09-26 13:51:08 -0500

    Update Profile.js

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,1 +1,19 @@
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class FeedScreen extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Profile</Text>
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+  },
+})
